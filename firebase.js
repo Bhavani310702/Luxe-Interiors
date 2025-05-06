90e7a36fa14474de897bdc8e900039e7c8cad782
Bhavani310702 <rramyanani@gmail.com>
2025-05-06 22:22:08 +0530

    adding

--- firebase.js
+++ firebase.js
@@ -1,26 +1,20 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
-  import { getAuth ,createUserWithEmailAndPassword ,signInWithEmailAndPassword  } from 'https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js'
-//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import {  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBK1C8kr7P3NZel9MxauvF-GWiebm1jgQc",
-    authDomain: "e-commerce-c3a05.firebaseapp.com",
-    projectId: "e-commerce-c3a05",
-    storageBucket: "e-commerce-c3a05.firebasestorage.app",
-    messagingSenderId: "533414333162",
-    appId: "1:533414333162:web:f30d7fc52bc7be6bd76593",
-    measurementId: "G-RZVZ66XDTR"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDCKFef7NxgM5eFWsDtEt3Oo9uypiS8RZU",
+  authDomain: "e-commerce-3a0d0.firebaseapp.com",
+  projectId: "e-commerce-3a0d0",
+  storageBucket: "e-commerce-3a0d0.firebasestorage.app",
+  messagingSenderId: "459591229566",
+  appId: "1:459591229566:web:ead0c22c2d66dc03f5782f",
+  measurementId: "G-0J6VX41NNH"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app)
-
-  export{auth ,createUserWithEmailAndPassword ,signInWithEmailAndPassword }
-
-//   const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export {auth, createUserWithEmailAndPassword,signInWithEmailAndPassword}
